4c5cdf056d3fbb89ed2dfd859ed9579cc6ae1344
Andrew <njayaandrew@gmail.com>
2021-02-16 03:56:51 +0300

    [chore] added json and browsable api default render classes

--- Store/settings.py
+++ Store/settings.py
@@ -70,6 +70,10 @@ ROOT_URLCONF = 'Store.urls'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ]
 }
 
